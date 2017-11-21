97b4ebb17c8c88b9400c143fcef8a4ca7e5c69fa
Akash Sheth <absheth@iu.edu>
2017-11-21 00:00:46 -0500

    Some corrections and refreshing

---
+++ RubyMonk/Beginner/learning.rb
@@ -0,0 +1,6 @@
+s = "akash sheth"
+
+
+puts "My name is #{s}"
+
+puts "This is a new string".upcase
--- RubyMonk/Beginner/rectangle.rb
+++ RubyMonk/Beginner/rectangle.rb
@@ -18,3 +18,16 @@ abcd =  1.method("next").call
 puts abcd
 
 reverse_sign(0)
+
+def do_nothing
+end
+
+puts do_nothing
+
+def do_something
+  return
+  puts "Akash Sheth is a good boy."
+end
+
+
+puts do_something.class
